292e135d61e950aca3dba971f0a060b7b3503cb4
Jakub Szymczak <jakubszymczak@Jakubs-MacBook-Pro.local>
2020-12-06 09:08:22 +0000

    Adding ex002

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,14 +1,23 @@
 package com.techreturners.exercise002;
+import java.util.*;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
+    public boolean isFromManchester(Person person) {
+        String city = person.getCity();
+        if (city == "Manchester") { 
+            return true;
+            
+        }
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
+    public boolean canWatchFilm(Person person, int limit) {
+        int age = person.getAge();
+        if (age >= limit) { 
+            return true;
+            
+        }
         return false;
     }
     
